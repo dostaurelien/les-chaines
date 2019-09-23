6a65fdf950051e1aeb546e550afaf06ad4cb76fa
dostaurelien <54935526+dostaurelien@users.noreply.github.com>
2019-09-23 17:42:30 +0200

    Create php2.php

---
+++ php2.php
@@ -0,0 +1,45 @@
+<?php
+$message1= '0@sn9sirppa@#?ia\'jgtvryko1';
+
+$length= strlen($message1)/2;
+//echo $length;
+
+//echo substr ( $message1 , 5 , $length );
+ $message1 = substr ( $message1 , 5 , $length );
+
+
+//echo str_replace ('@#?',' ' ,$message1);
+
+$message1= str_replace ('@#?', ' ' ,$message1);
+
+echo strrev ( $message1 ). " ";
+
+$message2= 'q8e?wsellecif@#?sel@#?setuotpazdsy0*b9+mw@x1vj';
+
+$length= strlen($message2)/2;
+//echo $length;
+
+//echo substr ( $message1 , 5 , $length );
+ $message2 = substr ( $message2 , 5 , $length );
+
+
+//echo str_replace ('@#?',' ' ,$message1);
+
+$message2= str_replace ('@#?', ' ' ,$message2);
+
+echo strrev ( $message2 ). " ";
+
+$message3= 'aopi?sgnirts@#?sedhtg+p9l!';
+
+$length= strlen($message3)/2;
+//echo $length;
+
+//echo substr ( $message1 , 5 , $length );
+ $message3 = substr ( $message3 , 5 , $length );
+
+
+//echo str_replace ('@#?',' ' ,$message1);
+
+$message3= str_replace ('@#?', ' ' ,$message3);
+
+echo strrev ( $message3 );
